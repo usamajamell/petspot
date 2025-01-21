00cf6938d69791a8c11fe29f69396e952a809347
Usama Jamil <151847474+usamajamil2023@users.noreply.github.com>
2025-01-21 18:15:30 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+gsap.from(".hero-heading",{
+    y:400,
+    opacity:0,
+    duration:2,
+    delay:0.5,
+    stagger:0.15
+})
+gsap.from("#getstrtd",{
+    y:400,
+    opacity:0,
+    duration:2,
+    delay:1.5,
+   
+})
+gsap.from("#sub-text",{
+    y:400,
+    opacity:0,
+    duration:2,
+    delay:1.5,
+   
+})
+
+gsap.from(".animal1",{
+    x:-1200,
+    duration:2,
+    delay:1,
+    
+})
+
+gsap.from(".animal2",{
+    x:1200,
+    duration:2,
+    delay:1,
+})
+gsap.from("#dogbone",{
+    y:1200,
+    x:1200,
+    duration:2,
+    delay:1,
+})
+gsap.from("#dogpaws",{
+    x:-600,
+    y:-600,
+    duration:2,
+    delay:1,
+})
